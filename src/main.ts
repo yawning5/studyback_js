77beb7818a4f9d1bee530b50562d211eae872c28
yawning5 <yawning055@gmail.com>
2025-06-12 23:42:25 +0900

    feat: CORS 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 설정
+  app.enableCors({
+    origin: 'http://localhost:3000', // 허용할 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
+    credentials: true, // 쿠키를 포함한 요청 허용
+  });
+
   const port = 3000;
   await app.listen(port);
   console.log(`🚀 Server listening on http://localhost:${port}`);
